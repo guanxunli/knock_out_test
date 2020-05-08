87c3b124328765d003902c4833863159ae76bda0
Guanxun Li <victorguanxun@gmail.com>
2020-05-08 14:08:22 -0500

    Add imaginary test.

--- simulation data/ima_try.R
+++ simulation data/ima_try.R
@@ -1,13 +1,7 @@
 library(ggplot2)
-library(statsExpressions)
-library(patchwork)
-library(circlize)
-library(pbapply)
 library(scTenifoldNet)
 
-col_fun = colorRamp2(c(-1, 0, 1), c("blue", "white", "red"))
-
-SERGIO <- read.csv('SERGIO/simulationOutput.csv', header = FALSE)
+SERGIO <- read.csv('simulation data/SERGIO_create_sim_data/simulationOutput.csv', header = FALSE)
 rownames(SERGIO) <- paste0('G', seq_len(nrow(SERGIO)))
 colnames(SERGIO) <- paste0('C', seq_len(ncol(SERGIO)))
 
@@ -18,38 +12,89 @@ X <- scTenifoldNet::tensorDecomposition(X)
 X <- X$X
 X <- as.matrix(X)
 
-manifoldAlignment <- function(X, Y, d = 30){
+manifoldAlignment <- function (X, Y, d = 30) {
   sharedGenes <- intersect(rownames(X), rownames(Y))
   X <- X[sharedGenes, sharedGenes]
   Y <- Y[sharedGenes, sharedGenes]
+  L <- diag(length(sharedGenes))
   wX <- X + 1
   wY <- Y + 1
-  L <- diag(length(sharedGenes))
-  wXY <- 0.9 * (sum(wX) + sum(wY)) / (2 * sum(L)) * L
+  wXY <- 0.9 * (sum(wX) + sum(wY))/(2 * sum(L)) * L
   W <- rbind(cbind(wX, wXY), cbind(t(wXY), wY))
-  W <- (W + t(W))/2
   W <- -W
   diag(W) <- 0
   diag(W) <- -apply(W, 2, sum)
-  # E <- suppressWarnings(RSpectra::eigs_sym(W, d*2))
-  E <- suppressWarnings(RSpectra::eigs(W, d*2, which = "SR"))
+  E <- suppressWarnings(RSpectra::eigs(W, d * 2, "SM"))
   E$values <- suppressWarnings(as.numeric(E$values))
-  E$vectors <- suppressWarnings(apply(E$vectors,2,as.numeric))
   newOrder <- order(E$values)
   E$values <- E$values[newOrder]
-  E$vectors <- E$vectors[,newOrder]
-  E$vectors <- E$vectors[,E$values > 1e-8]
-  alignedNet <- E$vectors[,seq_len(d)]
-  colnames(alignedNet) <- paste0('NLMA ', seq_len(d))
-  rownames(alignedNet) <- c(paste0('X_', sharedGenes), paste0('Y_', sharedGenes))
+  E$vectors <- E$vectors[, newOrder]
+  E$vectors <- E$vectors[, E$values > 1e-08]
+  alignedNet <- E$vectors[, 1:d]
+  colnames(alignedNet) <- paste0("NLMA ", seq_len(d))
+  rownames(alignedNet) <- c(paste0("X_", sharedGenes), 
+                            paste0("Y_", sharedGenes))
   return(alignedNet)
+}
+
+dRegulation <- function (manifoldOutput) {
+  geneList <- rownames(manifoldOutput)
+  geneList <- geneList[grepl("^X_", geneList)]
+  geneList <- gsub("^X_", "", geneList)
+  nGenes <- length(geneList)
+  eGenes <- nrow(manifoldOutput)/2
+  eGeneList <- rownames(manifoldOutput)
+  eGeneList <- eGeneList[grepl("^Y_", eGeneList)]
+  eGeneList <- gsub("^Y_", "", eGeneList)
+  if (nGenes != eGenes) {
+    stop("Number of identified and expected genes are not the same")
+  }
+  if (!all(eGeneList == geneList)) {
+    stop("Genes are not ordered as expected. X_ genes should be followed by Y_ genes in the same order")
+  }
+  dMetric <- sapply(seq_len(nGenes), function(G) {
+    X <- manifoldOutput[G, ]
+    Y <- manifoldOutput[(G + nGenes), ]
+    X_real <- Re(X)
+    X_im <- Im(X)
+    Y_real <- Re(Y)
+    Y_im <- Im(Y)
+    I_real <- rbind(X_real, Y_real)
+    I_im  <- rbind(X_im, Y_im)
+    O_real <- dist(I_real)
+    O_im <- dist(I_im)
+    O_real <- as.numeric(O_real)
+    O_im <- as.numeric(O_im)
+    O <- sqrt(O_real^2 + O_im^2)
+    return(O)
+  })
+  lambdaValues <- seq(-2, 2, length.out = 1000)
+  lambdaValues <- lambdaValues[lambdaValues != 0]
+  BC <- MASS::boxcox(dMetric ~ 1, plot = FALSE, lambda = lambdaValues)
+  BC <- BC$x[which.max(BC$y)]
+  if (BC < 0) {
+    nD <- 1/(dMetric^BC)
+  }
+  else {
+    nD <- dMetric^BC
+  }
+  Z <- scale(nD)
+  E <- mean(dMetric^2)
+  FC <- dMetric^2/E
+  pValues <- pchisq(q = FC, df = 1, lower.tail = FALSE)
+  pAdjusted <- p.adjust(pValues, method = "fdr")
+  dOut <- data.frame(gene = geneList, distance = dMetric, Z = Z, 
+                     FC = FC, p.value = pValues, p.adj = pAdjusted)
+  dOut <- dOut[order(dOut$p.value), ]
+  dOut <- as.data.frame.array(dOut)
+  return(dOut)
 }
 
 #### original method
 p_value_KO <- function(gKO){
   Y <- X
   Y[gKO,] <- 0
-  MA <- scTenifoldNet::manifoldAlignment(X,Y)
+  MA <- scTenifoldNet::manifoldAlignment(X,Y, d = 30)[, 1:10]
   DR <- scTenifoldNet::dRegulation(MA)
   DR$FC <- DR$distance^2/mean(DR$distance[-seq_len(length(gKO))]^2)
   DR$p.value <- pchisq(DR$FC, df = 1, lower.tail = FALSE)
@@ -83,12 +128,9 @@ length(intersect(g_list, g_true))
 p_value_KO <- function(gKO){
   Y <- X
   Y[gKO,] <- 0
-  #Y[, gKO] <- 0
-  MA <- manifoldAlignment(X,Y, d = 35)
-  #MA <- scTenifoldNet::manifoldAlignment(X,Y, d = 10)
-  DR <- scTenifoldNet::dRegulation(MA)
+  MA <- manifoldAlignment(X,Y, d = 10)
+  DR <- dRegulation(MA)
   DR$FC <- DR$distance^2/mean(DR$distance[-seq_len(length(gKO))]^2)
-  # DR$FC <- DR$distance^2/mean(DR$distance^2)
   DR$p.value <- pchisq(DR$FC, df = 1, lower.tail = FALSE)
   DR$p.adj <- p.adjust(DR$p.value, method = 'fdr')
   DR <- DR[DR$p.value < 0.05, ]
--- simulation data/yan_try.R
+++ simulation data/yan_try.R
@@ -22,12 +22,39 @@ manifoldAlignment <- function(X, Y, d = 30){
   sharedGenes <- intersect(rownames(X), rownames(Y))
   X <- X[sharedGenes, sharedGenes]
   Y <- Y[sharedGenes, sharedGenes]
-  wX <- X + 1
-  wY <- Y + 1
+  X <- X+1
+  Y <- Y+1
+  # wX <-  X %*% t(X) #+t(X) %*% X +
+  # wY <- Y %*% t(Y) #+ t(Y) %*% Y +
+  # diag(wX) <- 0
+  # diag(wY) <- 0
+  # wX = wX / max(wX)
+  # wY = wY / max(wY)
+  alpha = 0.5
+  beta = 0.5
+  indegreeX = rowSums(X) + 1
+  outdegreeX = colSums(X) + 1
+  Do = diag(outdegreeX^(-alpha))
+  Di = diag(indegreeX^(-beta))
+  Bd = Do %*% X %*% Di %*% t(X) %*%Do
+  Cd = Di %*% t(X) %*% Do %*% X %*%Di
+  wX = Bd + Cd
+  
+  indegreeY = rowSums(Y) + 1
+  outdegreeY = colSums(Y) + 1
+  Do = diag(outdegreeY^(-alpha))
+  Di = diag(indegreeY^(-beta))
+  Bd = Do %*% Y %*% Di %*% t(Y) %*%Do
+  Cd = Di %*% t(Y) %*% Do %*% Y %*%Di
+  wY = Bd + Cd
+  
+  wX = wX / max(wX)
+  wY = wY / max(wY)
   L <- diag(length(sharedGenes))
+  
   wXY <- 0.9 * (sum(wX) + sum(wY)) / (2 * sum(L)) * L
   W <- rbind(cbind(wX, wXY), cbind(t(wXY), wY))
-  W <- (W + t(W))/2
+  # W <- (W + t(W))/2
   W <- -W
   diag(W) <- 0
   diag(W) <- -apply(W, 2, sum)
@@ -45,39 +72,68 @@ manifoldAlignment <- function(X, Y, d = 30){
   return(alignedNet)
 }
 
-#### original method
-p_value_KO <- function(gKO){
-  Y <- X
-  Y[gKO,] <- 0
-  MA <- scTenifoldNet::manifoldAlignment(X,Y)
-  DR <- scTenifoldNet::dRegulation(MA)
-  DR$FC <- DR$distance^2/mean(DR$distance[-seq_len(length(gKO))]^2)
-  DR$p.value <- pchisq(DR$FC, df = 1, lower.tail = FALSE)
-  DR$p.adj <- p.adjust(DR$p.value, method = 'fdr')
-  DR <- DR[DR$p.value < 0.05, ]
-  return(DR[order(DR$p.value, decreasing = FALSE), ])
-}
+# manifoldAlignment <- function(X, Y, d = 30){
+#   sharedGenes <- intersect(rownames(X), rownames(Y))
+#   X <- X[sharedGenes, sharedGenes]
+#   Y <- Y[sharedGenes, sharedGenes]
+#   L <- diag(length(sharedGenes))
+#   X <- (X + t(X))/2
+#   Y <- (Y + t(Y))/2
+#   wX <- X+1
+#   wY <- Y+1
+#   wXY <- 0.9 * (sum(wX) + sum(wY)) / (2 * sum(L)) * L
+#   W <- rbind(cbind(wX, wXY), cbind(t(wXY), wY))
+#   # W <- (W + t(W))/2
+#   W <- -W
+#   diag(W) <- 0
+#   diag(W) <- -apply(W, 2, sum)
+#   # E <- suppressWarnings(RSpectra::eigs_sym(W, d*2))
+#   E <- suppressWarnings(RSpectra::eigs(W, d*2, which = "SR"))
+#   E$values <- suppressWarnings(as.numeric(E$values))
+#   E$vectors <- suppressWarnings(apply(E$vectors,2,as.numeric))
+#   newOrder <- order(E$values)
+#   E$values <- E$values[newOrder]
+#   E$vectors <- E$vectors[,newOrder]
+#   E$vectors <- E$vectors[,E$values > 1e-8]
+#   alignedNet <- E$vectors[,seq_len(d)]
+#   colnames(alignedNet) <- paste0('NLMA ', seq_len(d))
+#   rownames(alignedNet) <- c(paste0('X_', sharedGenes), paste0('Y_', sharedGenes))
+#   return(alignedNet)
+# }
 
-g_res <- p_value_KO(20)
-g_list <- g_res$gene
-g_true <- paste0("G", 16:40)
-length(g_list)
-length(g_true)
-length(intersect(g_list, g_true))
-
-g_res <- p_value_KO(50)
-g_list <- g_res$gene
-g_true <- paste0("G", 41:80)
-length(g_list)
-length(g_true)
-length(intersect(g_list, g_true))
-
-g_res <- p_value_KO(100)
-g_list <- g_res$gene
-g_true <- paste0("G", 81:100)
-length(g_list)
-length(g_true)
-length(intersect(g_list, g_true))
+# #### original method
+# p_value_KO <- function(gKO){
+#   Y <- X
+#   Y[gKO,] <- 0
+#   MA <- scTenifoldNet::manifoldAlignment(X,Y)
+#   DR <- scTenifoldNet::dRegulation(MA)
+#   DR$FC <- DR$distance^2/mean(DR$distance[-seq_len(length(gKO))]^2)
+#   DR$p.value <- pchisq(DR$FC, df = 1, lower.tail = FALSE)
+#   DR$p.adj <- p.adjust(DR$p.value, method = 'fdr')
+#   DR <- DR[DR$p.value < 0.05, ]
+#   return(DR[order(DR$p.value, decreasing = FALSE), ])
+# }
+#
+# g_res <- p_value_KO(20)
+# g_list <- g_res$gene
+# g_true <- paste0("G", 16:40)
+# length(g_list)
+# length(g_true)
+# length(intersect(g_list, g_true))
+#
+# g_res <- p_value_KO(50)
+# g_list <- g_res$gene
+# g_true <- paste0("G", 41:80)
+# length(g_list)
+# length(g_true)
+# length(intersect(g_list, g_true))
+#
+# g_res <- p_value_KO(100)
+# g_list <- g_res$gene
+# g_true <- paste0("G", 81:100)
+# length(g_list)
+# length(g_true)
+# length(intersect(g_list, g_true))
 
 #### setting row as zero
 p_value_KO <- function(gKO){
