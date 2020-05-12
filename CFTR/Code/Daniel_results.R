92a074e59ebbfdb230d3f7cb886c1e4de6a5d144
Guanxun Li <victorguanxun@gmail.com>
2020-05-11 20:53:33 -0500

    Add tests for CFTR.

--- CFTR/Code/Daniel_results.R
+++ CFTR/Code/Daniel_results.R
@@ -5,9 +5,9 @@ library(igraph)
 source("CFTR/Code/utility.R")
 
 ## inpu data
-MA_ori_row0 <- readRDS("CFTR/results/Daniel_results/Daniel_ori_row0.rds")
-MA_ori_col0 <- readRDS("CFTR/results/Daniel_results/Daniel_ori_col0.rds")
-MA_ori_both0 <- readRDS("CFTR/results/Daniel_results/Daniel_ori_both0.rds")
+X_ori_row0 <- readRDS("CFTR/results/Daniel_results/Daniel_ori_row0.rds")
+X_ori_col0 <- readRDS("CFTR/results/Daniel_results/Daniel_ori_col0.rds")
+X_ori_both0 <- readRDS("CFTR/results/Daniel_results/Daniel_ori_both0.rds")
 
 ######################################################################################################
 ################################## check results                 #####################################
@@ -15,25 +15,25 @@ MA_ori_both0 <- readRDS("CFTR/results/Daniel_results/Daniel_ori_both0.rds")
 
 ## check sensitivity
 out_ori_row <- check_sensitivity(X = X_ori_row0, d_index = c(1:10))
-saveRDS(out_ori_row, file = "TREM2/results/Daniel_results/out_ori_row.rds")
+saveRDS(out_ori_row, file = "CFTR/results/Daniel_results/out_ori_row.rds")
 out_ori_col <- check_sensitivity(X = X_ori_col0, d_index = c(1:10))
-saveRDS(out_ori_col, file = "TREM2/results/Daniel_results/out_ori_col.rds")
+saveRDS(out_ori_col, file = "CFTR/results/Daniel_results/out_ori_col.rds")
 out_ori_both <- check_sensitivity(X = X_ori_both0, d_index = c(1:10))
-saveRDS(out_ori_both, file = "TREM2/results/Daniel_results/out_ori_both.rds")
+saveRDS(out_ori_both, file = "CFTR/results/Daniel_results/out_ori_both.rds")
 
 ## plot sensitivity
-out_ori_row0 <- readRDS("TREM2/results/Daniel_results/out_ori_row.rds")
+out_ori_row0 <- readRDS("CFTR/results/Daniel_results/out_ori_row.rds")
 plot_sencitivity(out_ori_row0)
-out_ori_col0 <- readRDS("TREM2/results/Daniel_results/out_ori_col.rds")
+out_ori_col0 <- readRDS("CFTR/results/Daniel_results/out_ori_col.rds")
 plot_sencitivity(out_ori_col0)
-out_ori_both0 <- readRDS("TREM2/results/Daniel_results/out_ori_both.rds")
+out_ori_both0 <- readRDS("CFTR/results/Daniel_results/out_ori_both.rds")
 plot_sencitivity(out_ori_both0)
 
 ## check head gene and trivial method
-load("TREM2/Daniel_results/GSE130626.RData")
-WT <- GSE130626$WT
+load("CFTR/Daniel_results/SRS4245406.RData")
+WT <- SRS4245406$WT
 WT <- as.matrix(WT)
-gKO <- 'Trem2'
+gKO = 'Cftr'
 g_wei <- abs(WT[gKO, ])
 gList <- colnames(WT)[order(g_wei, decreasing = TRUE)]
 
@@ -54,3 +54,7 @@ length(out_ori_both0$gene)
 head(out_ori_both0$gene)
 n <- length(out_ori_both0$gene)
 print(c(n, length(intersect(gList[1:n], out_ori_both0$gene))))
+
+check_intersect(out_ori_row0$gene, out_ori_col0$gene)
+check_intersect(out_ori_row0$gene, out_ori_both0$gene)
+check_intersect(out_ori_col0$gene, out_ori_both0$gene)
--- CFTR/Code/MA_dir_results.R
+++ CFTR/Code/MA_dir_results.R
@@ -2,39 +2,137 @@ library(fgsea)
 library(ggplot2)
 library(enrichR)
 library(igraph)
-source("NKX2-1/Code/utility.R")
+source("CFTR/Code/utility.R")
 
 ## inpu data
-MA_both_nnorm <- readRDS("NKX2-1/results/MA_dir_results/MA_both_nnorm.rds")
-MA_row_nnorm <- readRDS("NKX2-1/results/MA_dir_results/MA_row_nnorm.rds")
-MA_col_nnorm <- readRDS("NKX2-1/results/MA_dir_results/MA_col_nnorm.rds")
-MA_both_norm <- readRDS("NKX2-1/results/MA_dir_results/MA_both_norm.rds")
-MA_row_norm <- readRDS("NKX2-1/results/MA_dir_results/MA_row_norm.rds")
-MA_col_norm <- readRDS("NKX2-1/results/MA_dir_results/MA_col_norm.rds")
-
-# marker gene
-markerGenes <- read.csv('NKX2-1/Data/pnas.1906663116.sd01.csv', stringsAsFactors = FALSE, row.names = 1)
-markerGenes$T.test.p.value <- as.numeric(markerGenes$T.test.p.value)
-markerGenes <- markerGenes[complete.cases(markerGenes),]
-markerGenesAT1 <- markerGenes$gene_short_name[markerGenes$T.test.p.value < 0.05]
-
-markerGenes <- read.csv('NKX2-1//Data/pnas.1906663116.sd05.csv', stringsAsFactors = FALSE)
-markerGenes <- markerGenes[,1:10]
-markerGenes$T.test.p.value <- as.numeric(markerGenes$T.test.p.value)
-markerGenes <- markerGenes[complete.cases(markerGenes),]
-markerGenesAT2 <- markerGenes$gene_short_name[markerGenes$T.test.p.value < 0.05]
-
-markerGenes <- unique(c(markerGenesAT1, markerGenesAT2)) # 7808
-length(markerGenes)
+MA_both_nnorm <- readRDS("CFTR/results/MA_dir_results/MA_both_nnorm.rds")
+MA_row_nnorm <- readRDS("CFTR/results/MA_dir_results/MA_row_nnorm.rds")
+MA_col_nnorm <- readRDS("CFTR/results/MA_dir_results/MA_col_nnorm.rds")
+MA_both_norm <- readRDS("CFTR/results/MA_dir_results/MA_both_norm.rds")
+MA_row_norm <- readRDS("CFTR/results/MA_dir_results/MA_row_norm.rds")
+MA_col_norm <- readRDS("CFTR/results/MA_dir_results/MA_col_norm.rds")
 
 ######################################################################################################
 ################################## check results                 #####################################
 ######################################################################################################
 
-out_row_nnorm <- check_fun(X = MA_row_nnorm, d = 5, alpha = 1)
-out_col_nnorm <- check_fun(X = MA_col_nnorm, d = 5, alpha = 2)
-out_both_nnorm <- check_fun(X = MA_both_nnorm, d = 5, alpha = 0)
+
+## sensitivity
+out_row_nnorm <- check_sensitivity(X = MA_row_nnorm, d_index = c(1:10), alpha = 2)
+saveRDS(out_row_nnorm, "CFTR/results/MA_dir_results/out_row_nnorm.rds")
+out_col_nnorm <- check_sensitivity(X = MA_col_nnorm, d_index = c(1:10), alpha = 1)
+saveRDS(out_col_nnorm, "CFTR/results/MA_dir_results/out_col_nnorm.rds")
+out_both_nnorm <- check_sensitivity(X = MA_both_nnorm, d_index = c(1:10), alpha = 0)
+saveRDS(out_both_nnorm, "CFTR/results/MA_dir_results/out_both_nnorm.rds")
+
+## using p-value rather than p-adj
+out_row_norm <- check_sensitivity(X = MA_row_norm, d_index = c(1:10), alpha = 2)
+saveRDS(out_row_norm, "CFTR/results/MA_dir_results/out_row_norm.rds")
+out_col_norm <- check_sensitivity(X = MA_col_norm, d_index = c(1:10), alpha = 1)
+saveRDS(out_col_norm, "CFTR/results/MA_dir_results/out_col_norm.rds")
+out_both_norm <- check_sensitivity(X = MA_both_norm, d_index = c(1:10), alpha = 0)
+saveRDS(out_both_norm, "CFTR/results/MA_dir_results/out_both_norm.rds")
+
+## plot results
+out_row_norm <- readRDS("CFTR/results/MA_dir_results/out_row_norm.rds")
+plot_sencitivity(out_row_norm)
+out_col_norm <- readRDS("CFTR/results/MA_dir_results/out_col_norm.rds")
+plot_sencitivity(out_col_norm)
+out_both_norm <- readRDS("CFTR/results/MA_dir_results/out_both_norm.rds")
+plot_sencitivity(out_both_norm)
+out_row_nnorm <- readRDS("CFTR/results/MA_dir_results/out_row_nnorm.rds")
+plot_sencitivity(out_row_nnorm)
+out_col_nnorm <- readRDS("CFTR/results/MA_dir_results/out_col_nnorm.rds")
+plot_sencitivity(out_col_nnorm)
+out_both_nnorm <- readRDS("CFTR/results/MA_dir_results/out_both_nnorm.rds")
+plot_sencitivity(out_both_nnorm)
+
+## check first d and last d
+out_row_nnorm_f <- check_fun(X = MA_row_nnorm, d = 2, alpha = 1)
+length(out_row_nnorm_f$gene)
+head(out_row_nnorm_f$gene)
+out_row_nnorm_b <- check_fun(X = MA_row_nnorm, d = 2, alpha = 2)
+head(out_row_nnorm_b$gene)
+length(out_row_nnorm_b$gene)
+
+out_col_nnorm_f <- check_fun(X = MA_col_nnorm, d = 2, alpha = 1)
+length(out_col_nnorm_f$gene)
+head(out_col_nnorm_f$gene)
+out_col_nnorm_b <- check_fun(X = MA_col_nnorm, d = 2, alpha = 2)
+head(out_col_nnorm_b$gene)
+length(out_col_nnorm_b$gene)
+
+out_both_nnorm_f <- check_fun(X = MA_both_nnorm, d = 2, alpha = 1)
+length(out_both_nnorm_f$gene)
+head(out_both_nnorm_f$gene)
+out_both_nnorm_b <- check_fun(X = MA_both_nnorm, d = 2, alpha = 2)
+head(out_both_nnorm_b$gene)
+length(out_both_nnorm_b$gene)
+
+out_row_norm_f <- check_fun(X = MA_row_norm, d = 2, alpha = 1)
+length(out_row_norm_f$gene)
+head(out_row_norm_f$gene)
+out_row_norm_b <- check_fun(X = MA_row_norm, d = 2, alpha = 2)
+head(out_row_norm_b$gene)
+length(out_row_norm_b$gene)
+
+out_col_norm_f <- check_fun(X = MA_col_norm, d = 2, alpha = 1)
+length(out_col_norm_f$gene)
+head(out_col_norm_f$gene)
+out_col_norm_b <- check_fun(X = MA_col_norm, d = 2, alpha = 2)
+head(out_col_norm_b$gene)
+length(out_col_norm_b$gene)
+
+out_both_norm_f <- check_fun(X = MA_both_norm, d = 2, alpha = 1)
+length(out_both_norm_f$gene)
+head(out_both_norm_f$gene)
+out_both_norm_b <- check_fun(X = MA_col_norm, d = 2, alpha = 2)
+head(out_both_norm_b$gene)
+length(out_both_norm_b$gene)
+
+## check with trivial method
+load("CFTR/Daniel_results/SRS4245406.RData")
+WT <- SRS4245406$WT
+WT <- as.matrix(WT)
+gKO = 'Cftr'
+g_wei <- abs(WT[gKO, ])
+gList <- colnames(WT)[order(g_wei, decreasing = TRUE)]
 
 out_row_norm <- check_fun(X = MA_row_norm, d = 2, alpha = 2)
-out_col_norm <- check_fun(X = MA_col_norm, d = 5, alpha = 1)
-out_both_norm <- check_fun(X = MA_both_norm, d = 5, alpha = 0)
+n <- length(out_row_norm$gene)
+print(c(n, length(intersect(gList[1:n], out_row_norm$gene))))
+
+out_col_norm <- check_fun(X = MA_col_norm, d = 2, alpha = 1)
+n <- length(out_col_norm$gene)
+print(c(n, length(intersect(gList[1:n], out_col_norm$gene))))
+
+out_both_norm <- check_fun(X = MA_both_norm, d = 2, alpha = 2)
+n <- length(out_both_norm$gene)
+print(c(n, length(intersect(gList[1:n], out_both_norm$gene))))
+
+out_row_nnorm <- check_fun(X = MA_row_nnorm, d = 5, alpha = 2)
+n <- length(out_row_nnorm$gene)
+print(c(n, length(intersect(gList[1:n], out_row_nnorm$gene))))
+
+out_col_nnorm <- check_fun(X = MA_col_nnorm, d = 5, alpha = 1)
+n <- length(out_col_nnorm$gene)
+print(c(n, length(intersect(gList[1:n], out_col_nnorm$gene))))
+
+out_both_nnorm <- check_fun(X = MA_both_nnorm, d = 5, alpha = 0)
+n <- length(out_both_nnorm$gene)
+print(c(n, length(intersect(gList[1:n], out_both_nnorm$gene))))
+
+## check intersection
+check_intersect(out_row_nnorm$gene, out_col_nnorm$gene)
+check_intersect(out_row_nnorm$gene, out_both_nnorm$gene)
+check_intersect(out_col_nnorm$gene, out_both_nnorm$gene)
+check_intersect(out_row_nnorm$gene, out_ori_row0$gene)
+check_intersect(out_row_nnorm$gene, out_ori_col0$gene)
+check_intersect(out_row_nnorm$gene, out_ori_both0$gene)
+check_intersect(out_col_nnorm$gene, out_ori_both0$gene)
+check_intersect(out_col_nnorm$gene, out_ori_col0$gene)
+check_intersect(out_col_nnorm$gene, out_ori_row0$gene)
+check_intersect(out_both_nnorm$gene, out_ori_both0$gene)
+check_intersect(out_both_nnorm$gene, out_ori_col0$gene)
+check_intersect(out_both_nnorm$gene, out_ori_row0$gene)
+
