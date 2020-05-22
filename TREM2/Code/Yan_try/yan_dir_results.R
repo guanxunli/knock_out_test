18cf0f2b31c758721f023c99d0c9931712603e05
Guanxun Li <victorguanxun@gmail.com>
2020-05-21 19:01:41 -0500

    Add one result.

--- TREM2/Code/Yan_try/yan_dir_results.R
+++ TREM2/Code/Yan_try/yan_dir_results.R
@@ -6,6 +6,7 @@ source("TREM2/Code/utility.R")
 
 ## inpu data
 yan_met_1_0 <- readRDS("TREM2/results/Yan_dir_results/Trem2yan_MA_row_Lt_1_0.rds")
+yan_met_1_1 <- readRDS("TREM2/results/Yan_dir_results/yan_MA_row_Lt_1_1.rds")
 
 ######################################################################################################
 ################################## check results                 #####################################
@@ -14,11 +15,16 @@ yan_met_1_0 <- readRDS("TREM2/results/Yan_dir_results/Trem2yan_MA_row_Lt_1_0.rds
 # sensitivity
 out_1_0 <- check_sensitivity(X = yan_met_1_0, d_index = c(1:10), alpha = 1)
 saveRDS(out_1_0, "TREM2/results/Yan_dir_results/out_1_0.rds")
+# sensitivity
+out_1_1 <- check_sensitivity(X = yan_met_1_1, d_index = c(1:10), alpha = 1)
+saveRDS(out_1_1, "TREM2/results/Yan_dir_results/out_1_1.rds")
 
 
 ## plot sensitivity
 out_1_0 <- readRDS("TREM2/results/Yan_dir_results/out_1_0.rds")
 plot_sencitivity(out_1_0)
+out_1_1 <- readRDS("TREM2/results/Yan_dir_results/out_1_1.rds")
+plot_sencitivity(out_1_1)
 
 ## check with trivial method 
 
@@ -33,14 +39,20 @@ gList_row <- colnames(WT)[which(g_wei > 0.2)]
 out_1_0 <- check_fun(X = yan_met_1_0, d = 2, alpha = 1)
 check_intersect(gList_row, out_1_0$gene)
 
+out_1_1 <- check_fun(X = yan_met_1_1, d = 1, alpha = 1)
+check_intersect(gList_row, out_1_1$gene)
+
 
 ## column trivial
 g_wei <- abs(WT[, gKO])
 gList_col <- colnames(WT)[which(g_wei > 0.2)]
 check_intersect(gList_col, out_1_0$gene)
+check_intersect(gList_col, out_1_1$gene)
 
 check_intersect(gList_row, gList_col)
 check_intersect(out_1_0$gene, out_ori_row0$gene)
+check_intersect(out_1_1$gene, out_ori_row0$gene)
 
 check_intersect(out_1_0$gene, out_row_sym$gene)
+check_intersect(out_1_1$gene, out_row_sym$gene)
 
