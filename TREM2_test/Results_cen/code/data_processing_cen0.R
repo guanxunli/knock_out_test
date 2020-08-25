c81022ac304023a67eadf68c47a6c8565c0614d0
Guanxun Li <victorguanxun@gmail.com>
2020-08-25 10:39:14 -0500

    Add new methods for Trem2.

---
+++ TREM2_test/Results_cen/code/data_processing_cen0.R
@@ -0,0 +1,192 @@
+library(fgsea)
+library(ggplot2)
+library(enrichR)
+library(igraph)
+source('https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/plotDR.R')
+source('https://raw.githubusercontent.com/dosorio/utilities/master/singleCell/plotKO.R')
+source('https://raw.githubusercontent.com/dosorio/utilities/master/idConvert/hsa2mmu_SYMBOL.R')
+
+dRegulation <- function(manifoldOutput, gKO){
+  
+  geneList <- rownames(manifoldOutput)
+  geneList <- geneList[grepl('^X_', geneList)]
+  geneList <- gsub('^X_','', geneList)
+  nGenes <- length(geneList)
+  
+  eGenes <- nrow(manifoldOutput)/2
+  
+  eGeneList <- rownames(manifoldOutput)
+  eGeneList <- eGeneList[grepl('^Y_', eGeneList)]
+  eGeneList <- gsub('^Y_','', eGeneList)
+  
+  if(nGenes != eGenes){
+    stop('Number of identified and expected genes are not the same')
+  }
+  if(!all(eGeneList == geneList)){
+    stop('Genes are not ordered as expected. X_ genes should be followed by Y_ genes in the same order')
+  }
+  
+  dMetric <- sapply(seq_len(nGenes), function(G){
+    X <- manifoldOutput[G,]
+    Y <- manifoldOutput[(G+nGenes),]
+    I <- rbind(X,Y)
+    O <- stats::dist(I)
+    O <- as.numeric(O)
+    return(O)
+  })
+  
+  ### BOX-COX
+  lambdaValues <- seq(-2,2,length.out = 1000)
+  lambdaValues <- lambdaValues[lambdaValues != 0]
+  BC <- MASS::boxcox(dMetric~1, plot=FALSE, lambda = lambdaValues)
+  BC <- BC$x[which.max(BC$y)]
+  if(BC < 0){
+    nD <- 1/(dMetric ^ BC)
+  } else {
+    nD <- dMetric ^ BC
+  }
+  Z <- scale(nD)
+  dOut <- data.frame(
+    gene = geneList, 
+    distance = dMetric,
+    Z = Z
+  )
+  dOut <- dOut[order(dOut$distance, decreasing = TRUE),]
+  FC <- (dOut$distance^2)/mean((dOut$distance[-seq_len(length(gKO))]^2))
+  pValues <- pchisq(q = FC,df = 1,lower.tail = FALSE)
+  pAdjusted <- p.adjust(pValues, method = 'fdr')
+  dOut$FC = FC
+  dOut$p.value = pValues
+  dOut$p.adj = pAdjusted
+  dOut <- as.data.frame.array(dOut)
+  return(dOut)
+}
+
+## plot DR
+GSE130626 <- readRDS("TREM2_test/Results_yan/trem2_yan.rds")
+WT <- readRDS("TREM2_test/Results_cen/data/WT.rds")
+KO <- WT
+KO["Trem2", ] <- 0
+MA <- readRDS("TREM2_test/Results_cen/data/ma_cen0.rds")
+GSE130626$WT <- WT
+GSE130626$KO <- KO
+GSE130626$manifoldalignment <- MA
+DR <- dRegulation(MA, "Trem2")
+GSE130626$diffRegulation <- DR
+dGenes <- GSE130626$diffRegulation$gene[GSE130626$diffRegulation$p.adj < 0.05]
+
+png('TREM2_test/Results_cen/results/dr_cen0_GSE130626.png', width = 2000, height = 2000, res = 300)
+plotDR(GSE130626)
+dev.off()
+
+## plot pathway
+png('TREM2_test/Results_cen/results/ego_cen0_GSE130626.png', width = 3000, height = 3000, res = 300, bg = NA)
+X <- GSE130626
+gKO <- 'Trem2'
+q <- 0.995
+gList <- unique(c(gKO, X$diffRegulation$gene[X$diffRegulation$p.adj < 0.05]))
+sCluster <- as.matrix(X$WT[gList,gList])
+koInfo <- sCluster[gKO,]
+sCluster[abs(sCluster) <= quantile(abs(sCluster), q)] <- 0
+sCluster[gKO,] <- koInfo
+diag(sCluster) <- 0
+sCluster <-  reshape2::melt(as.matrix(sCluster))
+colnames(sCluster) <- c('from', 'to', 'W')
+sCluster <- sCluster[sCluster$W != 0,]
+netPlot <- graph_from_data_frame(sCluster, directed = TRUE)
+dPlot <- centr_degree(netPlot)$res
+W <- rep(1,nrow(sCluster))
+sG   <- (names(V(netPlot))[dPlot > 1])[-1]
+W[sCluster$from %in% sG] <- 0.2
+W[sCluster$to %in% sG] <- 0.2
+W[sCluster$from %in% gKO] <- 1
+W[sCluster$from %in% gKO & sCluster$to %in% sG] <- 0.8
+set.seed(1)
+layPlot <- layout_with_fr(netPlot, weights = W)
+dPlot <- (dPlot/max(dPlot))*20
+E <- enrichr(gList, c("BioPlanet_2019", "KEGG_2019_Mouse", "Reactome_2016","GO_Biological_Process_2018", "GO_Molecular_Function_2018", "GO_Cellular_Component_2018"))
+E <- do.call(rbind.data.frame, E)
+E <- E[E$Adjusted.P.value < 0.05,]
+E <- E[order(E$Adjusted.P.value),]
+E$Term <- unlist(lapply(strsplit(E$Term,''), function(X){
+  X[1] <- toupper(X[1])
+  X <- paste0(X,collapse = '')
+  X <- gsub('\\([[:print:]]+\\)|Homo[[:print:]]+|WP[[:digit:]]+','',X)
+  X <- gsub("'s",'',X)
+  X <- unlist(strsplit(X,','))[1]
+  X <- gsub('[[:blank:]]$','',X)
+  return(X)
+}))
+E <- E[E$Term %in% c('Oxidative phosphorylation','Alzheimer disease','Cholesterol metabolism','Lysosome','Neutrophil mediated immunity'),]
+E <- E[c(1,3,5,6),]
+tPlot <- strsplit(E$Genes, ';')
+pPlot <- matrix(0,nrow = length(V(netPlot)), ncol = nrow(E))
+rownames(pPlot) <- toupper(names(V(netPlot)))
+for(i in seq_along(tPlot)){
+  pPlot[unlist(tPlot[i]),i] <- 1
+}
+pPlot <- lapply(seq_len(nrow(pPlot)), function(X){as.vector(pPlot[X,])})
+names(pPlot) <- names(V(netPlot))
+tPlot <- unique(unlist(tPlot))
+eGenes <- toupper(names(V(netPlot))) %in% tPlot
+vColor <- rgb(0,188/255,1,0.3)
+pieColors <- list(hcl.colors(nrow(E), palette = 'Zissou 1', alpha = 0.7))
+par(mar=c(4,0,0,0), xpd = TRUE)
+suppressWarnings(plot(netPlot,
+                      layout = layPlot,
+                      edge.arrow.size=.2,
+                      vertex.label.color="black",
+                      vertex.shape = ifelse(eGenes,'pie','circle'),
+                      vertex.pie = pPlot,
+                      vertex.size = 10+dPlot,
+                      vertex.pie.color=pieColors,
+                      vertex.label.family="Times",
+                      vertex.label.font=ifelse(eGenes,2,1),
+                      edge.color = ifelse(E(netPlot)$W > 0, 'red', 'blue'),
+                      edge.curved = ifelse(W == 0.2, 0, 0.1),
+                      vertex.color = vColor,
+                      vertex.frame.color = NA))
+sigLevel <- formatC(E$Adjusted.P.value, digits = 2, format = 'g', width = 0, drop0trailing = TRUE)
+gSetNames <- lengths(strsplit(E$Genes, ';'))
+gSetNames <- paste0('(', gSetNames,') ', E$Term, ' FDR = ', sigLevel)
+legend(x = -1.05, y = -1.05, legend = gSetNames, bty = 'n', ncol = 2, cex = 1, col = unlist(pieColors), pch = 16)
+dev.off()
+
+## gsea
+zGSE130626 <- GSE130626$diffRegulation$Z
+names(zGSE130626) <- toupper(GSE130626$diffRegulation$gene)
+
+MGI <- gmtPathways('https://amp.pharm.mssm.edu/Enrichr/geneSetLibrary?mode=text&libraryName=KEGG_2019_Mouse')
+E <- fgseaMultilevel(MGI, zGSE130626, absEps = 0)
+
+png('TREM2_test/Results_cen/results/gsea1_cen0_GSE130626.png', width = 1000, height = 1000, res = 300)
+gSet <- 'Cholesterol metabolism'
+plotEnrichment(MGI[[gSet]], zGSE130626) +
+  labs(
+    title = gSet,
+    subtitle = paste0('FDR = ', formatC(E$padj[E$pathway %in% gSet], digits = 2, format = 'e'))) +
+  xlab('Gene rank') +
+  ylab('Enrichment Score')
+dev.off()
+
+## gsea2
+png('TREM2_test/Results_cen/results/gsea2_cen0_GSE130626.png', width = 1000, height = 1000, res = 300)
+gSet <- 'Alzheimer disease'
+plotEnrichment(MGI[[gSet]], zGSE130626) +
+  labs(
+    title = gSet,
+    subtitle = paste0('FDR = ', formatC(E$padj[E$pathway %in% gSet], digits = 2, format = 'e'))) +
+  xlab('Gene rank') +
+  ylab('Enrichment Score')
+dev.off()
+
+## gsea3
+png('TREM2_test/Results_cen/results/gsea3_cen0_GSE130626.png', width = 1000, height = 1000, res = 300)
+gSet <- 'Lysosome'
+plotEnrichment(MGI[[gSet]], zGSE130626) +
+  labs(
+    title = gSet,
+    subtitle = paste0('FDR = ', formatC(E$padj[E$pathway %in% gSet], digits = 2, format = 'e'))) +
+  xlab('Gene rank') +
+  ylab('Enrichment Score')
+dev.off()
